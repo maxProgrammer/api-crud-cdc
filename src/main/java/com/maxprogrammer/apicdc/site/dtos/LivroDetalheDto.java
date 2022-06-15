287499c85710e596f39b2cd0d7e300683a9c362b
maxProgrammer <maxwneto.poo@gmail.com>
2022-06-15 17:42:37 -0300

    allowing markdown in bookdetails

--- src/main/java/com/maxprogrammer/apicdc/site/dtos/LivroDetalheDto.java
+++ src/main/java/com/maxprogrammer/apicdc/site/dtos/LivroDetalheDto.java
@@ -1,15 +1,17 @@
 package com.maxprogrammer.apicdc.site.dtos;
 
 import com.maxprogrammer.apicdc.models.Livro;
+import com.maxprogrammer.apicdc.shared.Markdown;
 
 import java.math.BigDecimal;
 
 public class LivroDetalheDto {
     private final String conteudo;
-    private final String sumario;
+    private final String sumarioOriginal;
     private final int paginas;
     private final String isbn;
     private final Long id;
+    private  String sumarioHtml;
     private BigDecimal preco;
     private String titulo;
     private String subtitulo;
@@ -19,7 +21,8 @@ public class LivroDetalheDto {
         subtitulo = livro.getSubTitulo();
         preco = livro.getPreco();
         conteudo = livro.getConteudo();
-        sumario = livro.getSumario();
+        sumarioOriginal = livro.getSumario();
+        sumarioHtml = Markdown.renderHtml(livro.getSumario());
         AutorLivroDetalheDto autor = new AutorLivroDetalheDto(livro.getAutor());
         paginas = livro.getNumeroPaginas();
         isbn = livro.getIsbn();
@@ -31,8 +34,12 @@ public class LivroDetalheDto {
         return conteudo;
     }
 
-    public String getSumario() {
-        return sumario;
+    public String getSumarioOriginal() {
+        return sumarioOriginal;
+    }
+
+    public String getSumarioHtml() {
+        return sumarioHtml;
     }
 
     public int getPaginas() {
